b0d6a9336fbb64606fb25be65180c029bad50eab
Asterios Raptis <asteri.raptis@gmail.com>
2024-08-03 14:03:22 +0200

    RequiredArgsConstructor issue fixed

--- src/main/java/io/github/astrapi69/crypt/data/model/AesRsaCryptModel.java
+++ src/main/java/io/github/astrapi69/crypt/data/model/AesRsaCryptModel.java
@@ -29,6 +29,7 @@ import java.io.Serializable;
 import lombok.AccessLevel;
 import lombok.Data;
 import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import lombok.experimental.SuperBuilder;
 
@@ -37,6 +38,7 @@ import lombok.experimental.SuperBuilder;
  * byte array of the encrypted object that is encrypted with the symmetric key
  */
 @Data
+@RequiredArgsConstructor
 @SuperBuilder(toBuilder = true)
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class AesRsaCryptModel implements Serializable
--- src/main/java/io/github/astrapi69/crypt/data/model/KeyInfo.java
+++ src/main/java/io/github/astrapi69/crypt/data/model/KeyInfo.java
@@ -27,6 +27,7 @@ package io.github.astrapi69.crypt.data.model;
 import lombok.AccessLevel;
 import lombok.Data;
 import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
 import lombok.experimental.SuperBuilder;
 
@@ -34,6 +35,7 @@ import lombok.experimental.SuperBuilder;
  * Data class representing key information
  */
 @Data
+@RequiredArgsConstructor
 @SuperBuilder(toBuilder = true)
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
 public class KeyInfo
